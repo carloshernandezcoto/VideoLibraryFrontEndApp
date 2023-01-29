6afdf4b0deb93c6222efd4c2bca7ff93985cbf8d
Carlos Hernandez <carlos.hernandezcoto@outlook.com>
2023-01-28 21:40:22 -0600

    First commit to github

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 
-console.log("SUPERLABEL", process.env.REACT_APP_NAME);
+console.log("SUPERLABEL", process.env.REACT_APP_API_URL);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
